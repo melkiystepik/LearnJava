6bcf3a981671291dc7e542611620872898cfd797
melkiystepik <stepigandruxer@gmail.com>
2023-07-29 11:33:16 +0300

    Commit direct from IDEA

--- src/learnJavaAtPrison/ImplementationTricks.java
+++ src/learnJavaAtPrison/ImplementationTricks.java
@@ -4,11 +4,11 @@ public class ImplementationTricks {
     public static void main(String[] args) {
         int primer=1;
         System.out.println("begining number is "+primer);
-        System.out.println("++i "+ (int)++primer);
-        System.out.println("i++ "+ (int)primer++);
-        System.out.println("i "+ (int)primer);
-        System.out.println("--i "+ (int)--primer);
-        System.out.println("i-- "+ (int)primer--);
+        System.out.println("++i "+ ++primer);
+        System.out.println("i++ "+ primer++);
+        System.out.println("i "+ primer);
+        System.out.println("--i "+ --primer);
+        System.out.println("i-- "+ primer--);
         System.out.println("i "+ primer);
     }
 }
